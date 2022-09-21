2dfbbaf21081bed351ba42d948b41a3b53a11f20
José Gabriel Corrêa <101403016+GhostWisher13@users.noreply.github.com>
2022-09-21 09:14:32 -0300

    Add files via upload

---
+++ APIexercicioCEP/cepformulario/src/App.vue
@@ -0,0 +1,53 @@
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      cep: "",
+      endereco: {},
+    };
+  },
+  methods: {
+    async buscar() {
+      const url = `https://viacep.com.br/ws/${this.cep}/json/`;
+      const res = await axios.get(url);
+      this.endereco = res.data;  
+    },
+  },
+};
+</script>
+
+<template>
+  <main>
+  <div class="container">
+    <h1>Busca CEP</h1>
+    <form @submit.prevent="buscar">
+      <input type="text" placeholder="CEP" v-model="cep" />
+      <button type="submit">Buscar</button>
+    </form>
+    <form class="end">
+      <label for="rua">Rua</label>
+      <input type="text" v-model="endereco.logradouro" />
+      <label for="rua">Bairro</label>
+      <input type="text" v-model="endereco.bairro" />
+      <label for="rua">Cidade</label>
+      <input type="text" v-model="endereco.localidade" />
+      <label for="rua">Estado</label>
+      <input type="text" v-model="endereco.uf" />
+      <label for="rua">DDD</label>
+      <input type="text" v-model="endereco.ddd" />
+    </form>
+  </div>
+  </main>
+</template>
+<style>
+.container {
+  background-color: #8c41aab2;
+  padding: 10px;
+  border-color: black;
+}
+.end {
+  display: flex;
+  flex-direction: column;
+}
+</style>
